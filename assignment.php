7b3a8155a2e9d509c64de1f496039cf9d3e58c32
Khin Chan Myae <130387473+Rumex92@users.noreply.github.com>
2023-06-04 17:37:27 +0630

    Add files via upload

---
+++ assignment.php
@@ -0,0 +1,81 @@
+
+<?php
+$age = 18;
+$citizenship = true;
+$ID = true;
+
+$eligibleToVote = ($age >= 18) && ($citizenship && $ID) ;
+
+echo "Age: $age<br>";
+echo "Citizenship: " . ($citizenship ? "Yes" : "No") . "<br>";
+echo "ID: " . ($ID ? "Yes" : "No") . "<br>";
+
+if ($eligibleToVote) {
+    echo "You are eligible to vote.<br>";
+} else {
+    echo "You are not eligible to vote.<br>";
+}
+?>
+
+<?php
+
+
+
+
+function isPrime($number)
+{
+    if ($number <= 1) {
+        return false;
+    }
+
+    for ($i = 2; $i <= sqrt($number); $i++) {
+        if ($number % $i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+echo '<br/>';
+
+function factorial($number)
+{
+    if ($number <= 1) {
+        return 1;
+    }
+
+    return $number * factorial($number - 1);
+}
+
+
+
+$randomNumber = rand(1, 100);
+
+if (isPrime($randomNumber)) {
+    echo "$randomNumber is a prime number.\n";
+} else {
+    echo "$randomNumber is not a prime number.\n";
+}
+echo '<br/>';
+
+
+echo "Factorial of $randomNumber is: " . factorial($randomNumber) . "\n";
+echo '<br/>';
+
+echo "Even numbers from 1 to 10: ";
+for ($i = 1; $i <= 10; $i++) {
+    if ($i % 2 === 0) {
+        echo $i . " ";
+    }
+}
+
+echo "\n";
+echo '<br/>';
+
+
+?>
+
+
+
